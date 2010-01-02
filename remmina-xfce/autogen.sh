5f9755819246d3ac081b9a277ef4f2e5625ef80f
llyzs <llyzs@b6cfa94a-2857-405c-b0d6-536ef9fc39e1>
2010-01-02 04:07:42 +0000

    Some fixes and simplification for autotool scripts on trunk
    git-svn-id: https://remmina.svn.sourceforge.net/svnroot/remmina/trunk@80 b6cfa94a-2857-405c-b0d6-536ef9fc39e1

--- remmina-xfce/autogen.sh
+++ remmina-xfce/autogen.sh
@@ -1,18 +1,5 @@
 #! /bin/sh
 
-AM_INSTALLED_VERSION=$(automake --version | sed -e '2,$ d' -e 's/.* \([0-9]*\.[0-9]*\).*/\1/')
-
-if [ "$AM_INSTALLED_VERSION" != "1.10" \
-    -a "$AM_INSTALLED_VERSION" != "1.11" ];then
-	echo
-	echo "You must have automake 1.10 or 1.11 to prepare the GNU build system."
-	exit 1
-fi
-
-if [ ! -d "m4" ]; then
-    mkdir m4
-fi
-
 aclocal
 autoheader --force
 libtoolize -c --automake --force
