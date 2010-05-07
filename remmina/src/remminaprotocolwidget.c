e6ca25d30bb7e28bbd6194d6a7d02b756b6b3db2
llyzs <llyzs.vic@gmail.com>
2010-05-07 17:30:32 +0000

    Connection status dialog just need to use the 'name' field. old bug.
    git-svn-id: https://remmina.svn.sourceforge.net/svnroot/remmina/trunk@163 b6cfa94a-2857-405c-b0d6-536ef9fc39e1

--- remmina/src/remminaprotocolwidget.c
+++ remmina/src/remminaprotocolwidget.c
@@ -213,8 +213,7 @@ remmina_protocol_widget_open_connection (RemminaProtocolWidget *gp, RemminaFile 
     gp->priv->remmina_file = remminafile;
     gp->priv->scale =  remminafile->scale;
 
-    /* Show "server" instead of "name" for quick connect */
-    remmina_protocol_widget_show_init_dialog (gp, (remminafile->filename ? remminafile->name : remminafile->server));
+    remmina_protocol_widget_show_init_dialog (gp, remminafile->name);
 
     if (remminafile->shareprinter)
     {
