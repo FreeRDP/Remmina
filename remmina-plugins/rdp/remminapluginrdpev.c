c066058d2f727875213f96bf20c1fdf3b3820ead
llyzs <llyzs.vic@gmail.com>
2011-01-31 15:50:33 +0800

    RDP: update copyright info

--- remmina-plugins/rdp/remminapluginrdpev.c
+++ remmina-plugins/rdp/remminapluginrdpev.c
@@ -19,6 +19,8 @@
  * Boston, MA 02111-1307, USA.
  */
 
+/* X11 drawings were ported from xfreerdp */
+
 #include "remminapluginrdp.h"
 #include "remminapluginrdpev.h"
 #include "remminapluginrdpui.h"
