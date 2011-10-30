ec672aff955582b9d25ea623cab21b3b67e7ab90
Vic Lee <llyzs@163.com>
2011-10-30 23:51:15 +0800

    rdp: add update function skeleton.

--- remmina-plugins/rdp/remminapluginrdpev.c
+++ remmina-plugins/rdp/remminapluginrdpev.c
@@ -2,6 +2,7 @@
  * Remmina - The GTK+ Remote Desktop Client
  * Copyright (C) 2010 Jay Sorg
  * Copyright (C) 2010-2011 Vic Lee
+ * Copyright (C) 2011 Marc-Andre Moreau <marcandre.moreau@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
