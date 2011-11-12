3a4bb03d884229ae01109591a2e6bcca2bbaf4a8
Marc-André Moreau <marcandre.moreau@gmail.com>
2011-11-12 16:03:44 -0500

    cmake: added detection scripts for avahi, appindicator, ssh, and vte

---
+++ cmake/FindAVAHI.cmake
@@ -0,0 +1,35 @@
+# Remmina - The GTK+ Remote Desktop Client
+#
+# Copyright (C) 2011 Marc-Andre Moreau
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, 
+# Boston, MA 02111-1307, USA.
+
+find_library(AVAHI_LIBRARY_UI NAMES avahi-ui-gtk3)
+find_library(AVAHI_LIBRARY_CLIENT NAMES avahi-client)
+
+find_path(AVAHI_INCLUDE_DIR avahi-client/publish.h)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(AVAHI DEFAULT_MSG AVAHI_LIBRARY_UI AVAHI_LIBRARY_CLIENT AVAHI_INCLUDE_DIR)
+
+if(AVAHI_FOUND)
+	set(AVAHI_LIBRARIES ${AVAHI_LIBRARY_UI} ${AVAHI_LIBRARY_CLIENT})
+	set(AVAHI_INCLUDE_DIRS ${AVAHI_INCLUDE_DIR})
+endif()
+
+mark_as_advanced(AVAHI_INCLUDE_DIR AVAHI_LIBRARY)
+
---
+++ cmake/FindVTE.cmake
@@ -0,0 +1,38 @@
+# Remmina - The GTK+ Remote Desktop Client
+#
+# Copyright (C) 2011 Marc-Andre Moreau
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, 
+# Boston, MA 02111-1307, USA.
+
+pkg_check_modules(PC_VTE vte-2.90)
+
+find_path(VTE_INCLUDE_DIR NAMES vte/vte.h
+	HINTS ${PC_VTE_INCLUDEDIR} ${PC_VTE_INCLUDE_DIRS}
+	PATH_SUFFIXES vte-2.90)
+
+find_library(VTE_LIBRARY NAMES vte)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(VTE DEFAULT_MSG VTE_LIBRARY VTE_INCLUDE_DIR)
+
+if(VTE_FOUND)
+	set(VTE_LIBRARIES ${VTE_LIBRARY})
+	set(VTE_INCLUDE_DIRS ${VTE_INCLUDE_DIR})
+endif()
+
+mark_as_advanced(VTE_INCLUDE_DIR VTE_LIBRARY)
+
