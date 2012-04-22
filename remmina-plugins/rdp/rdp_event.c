3c3544f3c666dd622a8a698d51ba99fc2a3a45ba
Jean-Louis Dupond <jean-louis@dupond.be>
2012-04-22 23:08:47 +0200

    Also handle GDK_SCROLL_SMOOTH

--- remmina-plugins/rdp/rdp_event.c
+++ remmina-plugins/rdp/rdp_event.c
@@ -377,6 +377,15 @@ static gboolean remmina_rdp_event_on_scroll(GtkWidget* widget, GdkEventScroll* e
 		case GDK_SCROLL_DOWN:
 			flag = PTR_FLAGS_WHEEL | PTR_FLAGS_WHEEL_NEGATIVE | 0x0088;
 			break;
+		
+		case GDK_SCROLL_SMOOTH:
+			if (event->delta_y < 0)
+				flag = PTR_FLAGS_WHEEL | 0x0078;
+			if (event->delta_y > 0)
+				flag = PTR_FLAGS_WHEEL | PTR_FLAGS_WHEEL_NEGATIVE | 0x0088;
+			if (!flag)
+				return FALSE;
+			break;
 
 		default:
 			return FALSE;
