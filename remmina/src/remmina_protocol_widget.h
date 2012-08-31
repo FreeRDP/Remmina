1ecbcefaf79fb855a456ee8572c46e09bdf306ea
Jean-Louis Dupond <jean-louis@dupond.be>
2012-08-31 15:28:37 +0200

    Issue #68: Notify user when host key changed

--- remmina/src/remmina_protocol_widget.h
+++ remmina/src/remmina_protocol_widget.h
@@ -108,6 +108,7 @@ void remmina_protocol_widget_set_display(RemminaProtocolWidget *gp, gint display
 gint remmina_protocol_widget_init_authpwd(RemminaProtocolWidget *gp, RemminaAuthpwdType authpwd_type);
 gint remmina_protocol_widget_init_authuserpwd(RemminaProtocolWidget *gp, gboolean want_domain);
 gint remmina_protocol_widget_init_certificate(RemminaProtocolWidget* gp, const gchar* subject, const gchar* issuer, const gchar* fingerprint);
+gint remmina_protocol_widget_changed_certificate(RemminaProtocolWidget *gp, const gchar* subject, const gchar* issuer, const gchar* new_fingerprint, const gchar* old_fingerprint);
 gchar* remmina_protocol_widget_init_get_username(RemminaProtocolWidget *gp);
 gchar* remmina_protocol_widget_init_get_password(RemminaProtocolWidget *gp);
 gchar* remmina_protocol_widget_init_get_domain(RemminaProtocolWidget *gp);
