b9a3150d82e711059c7a8d120add37d659962c85
Antenore Gatta <antenore@simbiosi.org>
2014-09-03 12:46:41 +0200

    GTK3 upgrade - move from gtk_table to gtk_grid (layout usable)

--- remmina/src/remmina_public.c
+++ remmina/src/remmina_public.c
@@ -267,7 +267,8 @@ void remmina_public_create_group(GtkGrid *table, const gchar *group, gint row, g
 	widget = gtk_label_new(NULL);
 	gtk_widget_show(widget);
 	gtk_widget_set_size_request(widget, 15, -1);
-	gtk_grid_attach(GTK_GRID(table), widget, 0, row + 1, 1, 2);
+	//gtk_grid_attach(GTK_GRID(table), widget, 0, row + 1, 1, 2);
+	gtk_grid_attach(GTK_GRID(table), widget, 0, row + 1, 1, 1);
 }
 
 gchar*
