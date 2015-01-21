fad1a4fb1a91023d196f186d3c13838460eb0746
Muflone <webreg@vbsimple.net>
2015-01-21 01:29:49 +0100

    Fixed lines indentation

--- remmina/include/remmina/remmina_trace_calls.h
+++ remmina/include/remmina/remmina_trace_calls.h
@@ -41,11 +41,11 @@
 
 #define TRACE_CALL(text) \
 { \
-  GDateTime *datetime = g_date_time_new_now_local(); \
- 	gchar *sfmtdate = g_date_time_format(datetime, "%x %X"); \
-  g_print("%s Trace calls: %s\n", sfmtdate, text); \
-  g_free(sfmtdate); \
-  g_date_time_unref(datetime); \
+	GDateTime *datetime = g_date_time_new_now_local(); \
+	gchar *sfmtdate = g_date_time_format(datetime, "%x %X"); \
+	g_print("%s Trace calls: %s\n", sfmtdate, text); \
+	g_free(sfmtdate); \
+	g_date_time_unref(datetime); \
 }
 
 #else
