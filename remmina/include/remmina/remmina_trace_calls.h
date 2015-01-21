2a412564768b33868eed5110052cde43f0bac8d3
Muflone <webreg@vbsimple.net>
2015-01-21 01:24:27 +0100

    Added local date and time in TRACE_CALLS macro

--- remmina/include/remmina/remmina_trace_calls.h
+++ remmina/include/remmina/remmina_trace_calls.h
@@ -36,7 +36,18 @@
 #define __REMMINATRACECALLS_H__
 
 #ifdef  WITH_TRACE_CALLS
-#define TRACE_CALL(text) g_print("Trace calls: %s\n", text)
+
+#include <gtk/gtk.h>
+
+#define TRACE_CALL(text) \
+{ \
+  GDateTime *datetime = g_date_time_new_now_local(); \
+ 	gchar *sfmtdate = g_date_time_format(datetime, "%x %X"); \
+  g_print("%s Trace calls: %s\n", sfmtdate, text); \
+  g_free(sfmtdate); \
+  g_date_time_unref(datetime); \
+}
+
 #else
 #define TRACE_CALL(text) 
 #endif  /* _WITH_TRACE_CALLS_ */
