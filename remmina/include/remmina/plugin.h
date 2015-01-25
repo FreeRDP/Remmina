743aaa85bbe33072325739ece9ab88321f7a12e6
Muflone <webreg@vbsimple.net>
2015-01-25 20:57:28 +0100

    Implemented disable password storing for protocols VNC VNCI, NX and RDP, thanks to Francisco Javier (inode64)

--- remmina/include/remmina/plugin.h
+++ remmina/include/remmina/plugin.h
@@ -169,8 +169,8 @@ typedef struct _RemminaPluginService
     gboolean     (* protocol_plugin_start_xport_tunnel)   (RemminaProtocolWidget *gp, RemminaXPortTunnelInitFunc init_func);
     void         (* protocol_plugin_set_display)          (RemminaProtocolWidget *gp, gint display);
     gboolean     (* protocol_plugin_close_connection)     (RemminaProtocolWidget *gp);
-    gint         (* protocol_plugin_init_authpwd)         (RemminaProtocolWidget *gp, RemminaAuthpwdType authpwd_type);
-    gint         (* protocol_plugin_init_authuserpwd)     (RemminaProtocolWidget *gp, gboolean want_domain);
+    gint         (* protocol_plugin_init_authpwd)         (RemminaProtocolWidget *gp, RemminaAuthpwdType authpwd_type, gboolean allow_password_saving);
+    gint         (* protocol_plugin_init_authuserpwd)     (RemminaProtocolWidget *gp, gboolean want_domain, gboolean allow_password_saving);
     gint         (* protocol_plugin_init_certificate)     (RemminaProtocolWidget *gp, const gchar* subject, const gchar* issuer, const gchar* fingerprint);
     gint         (* protocol_plugin_changed_certificate)  (RemminaProtocolWidget *gp, const gchar* subject, const gchar* issuer, const gchar* new_fingerprint, const gchar* old_fingerprint);
     gchar*       (* protocol_plugin_init_get_username)    (RemminaProtocolWidget *gp);
