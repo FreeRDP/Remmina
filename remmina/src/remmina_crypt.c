14ea36edcaaaf013fdddd85f69995d326112a4a8
Muflone <muflone@vbsimple.net>
2015-01-29 11:12:16 +0100

    Added copyright on modified files

--- remmina/src/remmina_crypt.c
+++ remmina/src/remmina_crypt.c
@@ -1,6 +1,7 @@
 /*
  * Remmina - The GTK+ Remote Desktop Client
  * Copyright (C) 2009 - Vic Lee 
+ * Copyright (C) 2014-2015 Antenore Gatta, Fabio Castelli, Giovanni Panozzo
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
