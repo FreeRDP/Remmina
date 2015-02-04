a78d90bd3dac196b3e06b6172f8e3169b8bdc22a
Muflone <muflone@vbsimple.net>
2015-02-04 18:02:41 +0100

    Added plugin.h for tool_hello_world plugin

--- remmina-plugins/tool_hello_world/plugin.h
+++ remmina-plugins/tool_hello_world/plugin.h
@@ -1,7 +1,6 @@
 /*
  * Remmina - The GTK+ Remote Desktop Client
- * Copyright (C) 2010 Vic Lee 
- * Copyright (C) 2014-2015 Antenore Gatta, Fabio Castelli, Giovanni Panozzo
+ * Copyright (C) 2015 Antenore Gatta, Fabio Castelli, Giovanni Panozzo
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,47 +32,4 @@
  *
  */
 
-#include "common/remmina_plugin.h"
-#if GTK_VERSION == 3
-#  include <gtk/gtkx.h>
-#endif
-#include "plugin.h"
-
-static RemminaPluginService *remmina_plugin_service = NULL;
-
-/* Protocol plugin definition and features */
-static RemminaToolPlugin remmina_plugin_tool =
-{
-	REMMINA_PLUGIN_TYPE_TOOL,                     // Type
-	"HELLO",                                      // Name
-	"Hello world!",                               // Description
-	GETTEXT_PACKAGE,                              // Translation domain
-	VERSION,                                      // Version number
-	remmina_plugin_tool_activate                  // Plugin activation callback
-};
-
-/* Tool plugin activation */
-static void remmina_plugin_tool_activate(void)
-{
-	GtkDialog *dialog;
-	dialog = GTK_DIALOG(gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL | GTK_DIALOG_USE_HEADER_BAR,
-		GTK_MESSAGE_INFO, GTK_BUTTONS_OK, remmina_plugin_tool.description));
-	gtk_dialog_run(dialog);
-	gtk_widget_destroy(GTK_WIDGET(dialog));
-}
-
-G_MODULE_EXPORT gboolean remmina_plugin_entry(RemminaPluginService *service)
-{
-	TRACE_CALL("remmina_plugin_entry");
-	remmina_plugin_service = service;
-
-	bindtextdomain(GETTEXT_PACKAGE, REMMINA_LOCALEDIR);
-	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
-
-	if (!service->register_plugin((RemminaPlugin *) &remmina_plugin_tool))
-	{
-		return FALSE;
-	}
-
-	return TRUE;
-}
+static void remmina_plugin_tool_activate(void);
