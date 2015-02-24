5ee7afa9b8803cd3730e24da7b0e283e5a2dd1d6
Antenore Gatta <antenore@simbiosi.org>
2015-02-24 21:41:42 +0100

    Fixes FreeRDP/Remmina#473 - Customize button in the connection editor should show the resolutions dialog

--- remmina/src/remmina_file_editor.c
+++ remmina/src/remmina_file_editor.c
@@ -43,6 +43,7 @@
 #include "remmina_public.h"
 #include "remmina_pref.h"
 #include "remmina_connection_window.h"
+#include "remmina_string_list.h"
 #include "remmina_pref_dialog.h"
 #include "remmina_file.h"
 #include "remmina_file_manager.h"
@@ -428,6 +429,53 @@ static void remmina_file_editor_create_password(RemminaFileEditor* gfe, GtkWidge
 	}
 }
 
+/* Validate the inserted value for a new resolution */
+static gboolean remmina_file_editor_resolution_validation_func(const gchar *new_str, gchar **error)
+{
+	TRACE_CALL("remmina_file_editor_resolution_validation_func");
+	gint i;
+	gint width, height;
+	gboolean splitted;
+	gboolean result;
+
+	width = 0;
+	height = 0;
+	splitted = FALSE;
+	result = TRUE;
+	for (i = 0; new_str[i] != '\0'; i++)
+	{
+		if (new_str[i] == 'x')
+		{
+			if (splitted)
+			{
+				result = FALSE;
+				break;
+			}
+			splitted = TRUE;
+			continue;
+		}
+		if (new_str[i] < '0' || new_str[i] > '9')
+		{
+			result = FALSE;
+			break;
+		}
+		if (splitted)
+		{
+			height = 1;
+		}
+		else
+		{
+			width = 1;
+		}
+	}
+
+	if (width == 0 || height == 0)
+		result = FALSE;
+
+	if (!result)
+		*error = g_strdup(_("Please enter format 'widthxheight'."));
+	return result;
+}
 static void remmina_file_editor_update_resolution(GtkWidget* widget, RemminaFileEditor* gfe)
 {
 	TRACE_CALL("remmina_file_editor_update_resolution");
@@ -438,12 +486,18 @@ static void remmina_file_editor_update_resolution(GtkWidget* widget, RemminaFile
 static void remmina_file_editor_browse_resolution(GtkWidget* button, RemminaFileEditor* gfe)
 {
 	TRACE_CALL("remmina_file_editor_browse_resolution");
-	GtkWidget* widget;
 
-	widget = remmina_pref_dialog_new(REMMINA_PREF_OPTIONS_TAB, GTK_WINDOW(gfe));
-	gtk_widget_show(widget);
+	GtkDialog *dialog = remmina_string_list_new();
+	remmina_string_list_set_validation_func(remmina_file_editor_resolution_validation_func);
+	remmina_string_list_set_text(remmina_pref.resolutions, TRUE);
+	remmina_string_list_set_titles(_("Resolutions"), _("Configure the available resolutions"));
+	gtk_window_set_transient_for(GTK_WINDOW(dialog), GTK_WINDOW(gfe));
+	gtk_dialog_run(dialog);
+	g_free(remmina_pref.resolutions);
+	remmina_pref.resolutions = remmina_string_list_get_text();
+	g_signal_connect(G_OBJECT(dialog), "destroy", G_CALLBACK(remmina_file_editor_update_resolution), gfe);
+	gtk_widget_destroy(GTK_WIDGET(dialog));
 
-	g_signal_connect(G_OBJECT(widget), "destroy", G_CALLBACK(remmina_file_editor_update_resolution), gfe);
 }
 
 static void remmina_file_editor_create_resolution(RemminaFileEditor* gfe, const RemminaProtocolSetting* setting,
@@ -451,6 +505,7 @@ static void remmina_file_editor_create_resolution(RemminaFileEditor* gfe, const 
 {
 	TRACE_CALL("remmina_file_editor_create_resolution");
 	GtkWidget* widget;
+	GtkWidget* combo;
 	GtkWidget* hbox;
 	const gchar* resolution;
 
@@ -476,10 +531,10 @@ static void remmina_file_editor_create_resolution(RemminaFileEditor* gfe, const 
 
 	resolution = remmina_file_get_string(gfe->priv->remmina_file, "resolution");
 
-	widget = remmina_public_create_combo_text_d(remmina_pref.resolutions, resolution, NULL);
-	gtk_widget_show(widget);
-	gtk_box_pack_start(GTK_BOX(hbox), widget, TRUE, TRUE, 0);
-	gfe->priv->resolution_custom_combo = widget;
+	combo = remmina_public_create_combo_text_d(remmina_pref.resolutions, resolution, NULL);
+	gtk_widget_show(combo);
+	gtk_box_pack_start(GTK_BOX(hbox), combo, TRUE, TRUE, 0);
+	gfe->priv->resolution_custom_combo = combo;
 
 	widget = gtk_button_new_with_label("...");
 	gtk_widget_show(widget);
