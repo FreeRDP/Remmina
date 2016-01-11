66edbdd0511e25eff5e7fad27812a2b2723c0b91
Giovanni Panozzo <giovanni@panozzo.it>
2016-01-11 10:36:30 +0100

    Save password just after successful connection

--- remmina/src/remmina_connection_window.c
+++ remmina/src/remmina_connection_window.c
@@ -3135,6 +3135,9 @@ static void remmina_connection_object_on_connect(RemminaProtocolWidget* gp, Remm
 	GtkWidget* tab;
 	gint i;
 
+	/* This signal handler is called by a plugin where it's correctly connected
+	 * (and authenticated) */
+
 	if (!cnnobj->cnnhld)
 	{
 		cnnwin = remmina_connection_window_find(cnnobj->remmina_file);
@@ -3164,6 +3167,9 @@ static void remmina_connection_object_on_connect(RemminaProtocolWidget* gp, Remm
 		remmina_pref_add_recent(remmina_file_get_string(cnnobj->remmina_file, "protocol"),
 		                        remmina_file_get_string(cnnobj->remmina_file, "server"));
 	}
+
+	/* Save credentials */
+	remmina_file_save_group(cnnobj->remmina_file, REMMINA_SETTING_GROUP_CREDENTIAL);
 
 	if (!cnnhld->cnnwin)
 	{
