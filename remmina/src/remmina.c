be2c4582df6e21f4840d5dbf3baa2486bda1cd05
Antenore Gatta <antenore@simbiosi.org>
2016-01-20 22:40:43 +0100

    Git commit hash added to version
    Now remmina version show something like "1.2.0-rcgit.8 (git 194cc9d)"
    
    Having a git commit id helps to identify builds and eases
    debuggin.
    
    The downside is that GetGitRevisionDescription retriggers a cmake
    re-configuration after each git commit. But thats the only way to get correct
    version informations without having git hooks or similar.
    
    GetGitRevisionDescription is based on Ryan Pavlik cmake Modules and @bmiklautz
    patch 32390d7 in FreeRDP

--- remmina/src/remmina.c
+++ remmina/src/remmina.c
@@ -137,7 +137,7 @@ static gint remmina_on_command_line(GApplication *app, GApplicationCommandLine *
 
 	if (remmina_option_version)
 	{
-		g_print ("%s - Version %s\n", g_get_application_name (), VERSION);
+		g_print ("%s - Version %s (git %s)\n", g_get_application_name (), VERSION, GIT_REVISION);
 		executed = TRUE;
 		status = 1;
 	}
