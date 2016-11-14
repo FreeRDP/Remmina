88093741c546c61846f502cbed824941a9cd22e0
Antenore Gatta <antenore@simbiosi.org>
2016-11-14 22:41:21 +0100

    When 'Server' GtkComboBox is selected, TAB key doesn't work - fixed by @ppsshh

--- remmina/src/remmina_file_editor.c
+++ remmina/src/remmina_file_editor.c
@@ -384,7 +384,6 @@ static void remmina_file_editor_create_server(RemminaFileEditor* gfe, const Remm
 
 	s = remmina_pref_get_recent(plugin->name);
 	widget = remmina_public_create_combo_entry(s, remmina_file_get_string(gfe->priv->remmina_file, "server"), TRUE);
-	gtk_widget_set_can_focus (widget, TRUE);
 	gtk_widget_show(widget);
 	gtk_widget_set_tooltip_markup(widget, _(server_tips));
 	gtk_entry_set_activates_default(GTK_ENTRY(gtk_bin_get_child(GTK_BIN(widget))), TRUE);
