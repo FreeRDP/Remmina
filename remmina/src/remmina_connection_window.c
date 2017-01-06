56cddd9cc4be3ee410bd7287eb26888321ec15d8
Antenore Gatta <antenore@simbiosi.org>
2017-01-06 21:25:27 +0100

    Fixed wrong cast on cnnwin

--- remmina/src/remmina_connection_window.c
+++ remmina/src/remmina_connection_window.c
@@ -2879,8 +2879,9 @@ static gboolean remmina_connection_window_go_fullscreen(GtkWidget *widget, GdkEv
 #if GTK_CHECK_VERSION(3, 18, 0)
 	gtk_window_fullscreen_on_monitor(GTK_WINDOW(cnnhld->cnnwin),
 			gdk_screen_get_default (),
-			gdk_screen_get_monitor_at_window (gdk_screen_get_default (),
-				GDK_WINDOW(cnnhld->cnnwin)));
+			gdk_screen_get_monitor_at_window
+			(gdk_screen_get_default (), gtk_widget_get_window(GTK_WIDGET(cnnhld->cnnwin))
+			));
 #else
 	remmina_log_print("Cannot fullscreen on a specific monitor, feature available from GTK 3.18");
 	gtk_window_fullscreen(GTK_WINDOW(cnnhld->cnnwin));
