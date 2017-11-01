FreeRDP/Remmina

commit 56e40d6849da9fb54a8e5941a7132a383cd8e65e

Using #pragma once instead of the old header definition

remmina-plugins/telepathy/telepathy_channel_handler.h
@@ -32,8 +32,7 @@
  *
  */
 
-#ifndef __REMMINATPCHANNELHANDLER_H__
-#define __REMMINATPCHANNELHANDLER_H__
+#pragma once
 
 G_BEGIN_DECLS
 

remmina-plugins/common/remmina_plugin.h
@@ -33,8 +33,7 @@
  *
  */
 
-#ifndef __REMMINAPLUGINCOMMON_H__
-#define __REMMINAPLUGINCOMMON_H__
+#pragma once
 
 #include "config.h"
 #include <gtk/gtk.h>
@@ -98,5 +97,4 @@ remmina_get_available_xdisplay (void) \
 	return display; \
 }
 
-#endif /* __REMMINAPLUGINCOMMON_H__ */
 

remmina-plugins/nx/nx_plugin.h
@@ -1,6 +1,6 @@
 /*
  * Remmina - The GTK+ Remote Desktop Client
- * Copyright (C) 2010 Vic Lee 
+ * Copyright (C) 2010 Vic Lee
  * Copyright (C) 2014-2015 Antenore Gatta, Fabio Castelli, Giovanni Panozzo
  *
  * This program is free software; you can redistribute it and/or modify
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA  02110-1301, USA.
  *
  *  In addition, as a special exception, the copyright holders give
@@ -33,8 +33,7 @@
  *
  */
 
-#ifndef __REMMINAPLUGINNX_H__
-#define __REMMINAPLUGINNX_H__
+#pragma once
 
 #define GET_PLUGIN_DATA(gp) (RemminaPluginNxData*) g_object_get_data(G_OBJECT(gp), "plugin-data");
 
@@ -80,6 +79,4 @@ typedef struct _RemminaPluginNxData
 extern RemminaPluginService *remmina_plugin_nx_service;
 
 G_END_DECLS
-
-#endif
 

remmina-plugins/nx/nx_session.h
@@ -32,8 +32,7 @@
  *
  */
 
-#ifndef __REMMINANXSESSION_H__
-#define __REMMINANXSESSION_H__
+#pragma once
 
 G_BEGIN_DECLS
 
@@ -105,5 +104,4 @@ void remmina_nx_session_bye(RemminaNXSession *nx);
 
 G_END_DECLS
 
-#endif  /* __REMMINANXSESSION_H__  */
 

remmina-plugins/nx/nx_session_manager.h
@@ -1,6 +1,6 @@
 /*
  * Remmina - The GTK+ Remote Desktop Client
- * Copyright (C) 2010 Vic Lee 
+ * Copyright (C) 2010 Vic Lee
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA  02110-1301, USA.
  *
  *  In addition, as a special exception, the copyright holders give
@@ -32,14 +32,11 @@
  *
  */
 
-#ifndef __REMMINANXSESSIONMANAGER_H__
-#define __REMMINANXSESSIONMANAGER_H__
+#pragma once
 
 G_BEGIN_DECLS
 
 void remmina_nx_session_manager_start(RemminaProtocolWidget *gp);
 
 G_END_DECLS
-
-#endif
 

remmina-plugins/rdp/rdp_channels.h
@@ -33,8 +33,7 @@
  */
 
 
-#ifndef __REMMINA_RDP_CHANNELS_H
-#define __REMMINA_RDP_CHANNELS_H
+#pragma once
 
 #include <freerdp/freerdp.h>
 #include <freerdp/client/channels.h>
@@ -53,4 +52,3 @@ void remmina_rdp_OnChannelDisconnectedEventHandler(rdpContext* context, ChannelC
 
 G_END_DECLS
 
-#endif

remmina-plugins/rdp/rdp_cliprdr.h
@@ -34,8 +34,7 @@
  *
  */
 
-#ifndef __REMMINA_RDP_CLIPRDR_H__
-#define __REMMINA_RDP_CLIPRDR_H__
+#pragma once
 
 
 #include <freerdp/freerdp.h>
@@ -48,5 +47,3 @@ void remmina_rdp_channel_cliprdr_process(RemminaProtocolWidget* gp, wMessage* ev
 void remmina_rdp_event_process_clipboard(RemminaProtocolWidget* gp, RemminaPluginRdpUiObject* ui);
 CLIPRDR_FORMAT_LIST *remmina_rdp_cliprdr_get_client_format_list(RemminaProtocolWidget* gp);
 
-
-#endif

remmina-plugins/rdp/rdp_event.h
@@ -32,8 +32,7 @@
  *
  */
 
-#ifndef __REMMINA_RDP_EVENT_H__
-#define __REMMINA_RDP_EVENT_H__
+#pragma once
 
 G_BEGIN_DECLS
 
@@ -49,6 +48,4 @@ int remmina_rdp_event_queue_ui_sync_retint(RemminaProtocolWidget* gp, RemminaPlu
 void *remmina_rdp_event_queue_ui_sync_retptr(RemminaProtocolWidget* gp, RemminaPluginRdpUiObject* ui);
 
 G_END_DECLS
-
-#endif
 

commit 4e91cbbe67126c27d8902520ddd68a4cac20c990

telepathy: (error) #endif without #if
Regression from 56e40d6849da9fb54a8e5941a7132a383cd8e65e

remmina-plugins/telepathy/telepathy_channel_handler.h
@@ -42,6 +42,3 @@ remmina_tp_channel_handler_new(const gchar *account_path, const gchar *connectio
 			       GHashTable *channel_properties, DBusGMethodInvocation *context);
 
 G_END_DECLS
-
-#endif
-