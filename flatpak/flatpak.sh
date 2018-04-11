9e30fcec3faf3b8b12313b6e60ed922b74430895
朝歌 <amtlib-dot-dll@users.noreply.github.com>
2018-04-11 11:12:03 +0800

    Update flatpak.sh

--- flatpak/flatpak.sh
+++ flatpak/flatpak.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-flatpak-builder --arch=x86_64 --user --install-deps-from=flathub --sandbox --repo=repo build *.json
-flatpak-builder --arch=i386 --user --install-deps-from=flathub --sandbox --repo=repo build *.json
+flatpak-builder --arch=$FLATPAK_ARCH --user --install-deps-from=flathub --sandbox --repo=repo build *.json
