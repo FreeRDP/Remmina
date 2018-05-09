0ebab396c61ccbc8b6d4857becb3931d79f4c509
Denis Ollier <larchunix@users.noreply.github.com>
2018-05-09 10:45:12 +0200

    travis: fix build in fedora container

--- flatpak/flatpak.sh
+++ flatpak/flatpak.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-flatpak-builder --arch=$FLATPAK_ARCH --user --install-deps-from=flathub --sandbox --repo=repo build *.json
+flatpak-builder --arch=$FLATPAK_ARCH --user --disable-rofiles-fuse --install-deps-from=flathub --sandbox --repo=repo build *.json
