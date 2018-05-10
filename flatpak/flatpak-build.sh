1a9da557716364eaf649e587ff10b9cc520fb9ad
Denis Ollier <larchunix@users.noreply.github.com>
2018-05-10 12:06:24 +0200

    travis: build flatpak using branch from PR instead of next

--- flatpak/flatpak-build.sh
+++ flatpak/flatpak-build.sh
@@ -1,9 +1,36 @@
 #!/bin/sh -e
 
-flatpak-builder \
-    --user \
-    --arch="${FLATPAK_ARCH}" \
+flatpak_builder()
+{
+    flatpak-builder \
+        --user \
+        --arch="${FLATPAK_ARCH}" \
+        --repo=repo \
+        --sandbox \
+        "$@"
+}
+
+# Build everything but Remmina module
+flatpak_builder \
     --install-deps-from=flathub \
-    --repo=repo \
-    --sandbox \
-    app/ org.remmina.Remmina.json
+    --stop-at=remmina \
+    app/ flatpak/org.remmina.Remmina.json
+
+# Build Remmina module from local checkout
+flatpak build app/ \
+    cmake -G Ninja \
+          -DCMAKE_INSTALL_PREFIX:PATH=/app \
+          -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
+          -DWITH_MANPAGES:BOOL=OFF \
+          -DWITH_TELEPATHY:BOOL=OFF \
+          .
+
+flatpak build app/ \
+    ninja -v install
+
+# Finish Flatpak app
+flatpak_builder \
+    --disable-download \
+    --disable-updates \
+    --finish-only \
+    app/ flatpak/org.remmina.Remmina.json
