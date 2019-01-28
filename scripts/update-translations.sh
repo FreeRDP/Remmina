02ed83a73237f416dca0cc2b5642376781c6e844
Antenore Gatta <antenore@simbiosi.org>
2019-01-28 07:39:14 +0100

    Fixed pot generation

--- scripts/update-translations.sh
+++ scripts/update-translations.sh
@@ -21,13 +21,24 @@ xgettext --from-code=UTF-8 \
 	--keyword=g_dngettext:2,3 \
 	--add-comments \
 	--files-from=po/POTFILES.in \
-	--output=po/remmina.pot \
+	--output=po/remmina.temp.pot \
 	--package-version="v1.3.0" \
 	--package-name="Remmina" \
 	--copyright-holder="Antenore Gatta and Giovanni Panozzo" \
 	--msgid-bugs-address="admin@remmina.org"
 
 cd "$REMMINATOP"/po || exit 1
+
+# Set charset to UTF-8
+sed -i -e 's/charset=CHARSET/charset=UTF-8/g' remmina.temp.pot
+
+if diff -qI "POT-Creation-Date" remmina.temp.pot remmina.pot ; then
+	echo "No new strings to be translated"
+	rm remmina.temp.pot
+	exit 0
+fi
+
+mv remmina.temp.pot remmina.pot
 
 for i in *.po; do
     msgmerge -N --backup=off --update "$i" remmina.pot
