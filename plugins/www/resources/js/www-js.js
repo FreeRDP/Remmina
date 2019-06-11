c22c4ee167935be317edb38c8379f6bdd7fd6c64
Antenore Gatta <antenore@simbiosi.org>
2019-06-11 17:40:26 +0200

    Debugging iframe issues

--- plugins/www/resources/js/www-js.js
+++ plugins/www/resources/js/www-js.js
@@ -15,27 +15,56 @@ function setLoginFields(frame) {
     }
     if (pswdField !== undefined) {
         pswdField.forEach(function(pswdElement) {
-            pswdElement.value = 'PWDPLACEHOLDER';
-            formNode = pswdElement.form;
-            usrField = formNode.querySelectorAll("input[type='text']");
-            usrField.forEach(function(usrElement) {
-                if (usrElement.getAttribute('autocomplete') !== 'new-password') {
+            if (pswdElement.getAttribute('autocomplete') !== 'new-password') {
+                pswdElement.value = 'PWDPLACEHOLDER';
+                var pswdElementId = pswdElement.id;
+            }
+            if ((frame !== undefined) && (frame !== null)) {
+                // using reference to iframe (ifrm) obtained above
+                var ifrm = document.getElementById(frame);
+                //alert("Dealing with frame " + ifrm);
+                var win = ifrm.contentWindow; // reference to iframe's window
+                //alert("Windows is " + win);
+                // reference to document in iframe
+                var doc = ifrm.contentDocument? ifrm.contentDocument: ifrm.contentWindow.document;
+                //alert("Document is " + doc);
+                // reference to form named 'demoForm' in iframe
+                var iframeElement = doc.getElementById(pswdElementId);
+                //alert("iframeElement " + iframeElement);
+                formNode = doc.getElementById(iframeElement.id);
+
+                //formNode = document.getElementById(frame).contentWindow.getElementById(pswdElementId).form;
+                alert("FormNode: " + formNode);
+                usrField = formNode.querySelectorAll("input[type='text']");
+                alert(usrField);
+            } else {
+                formNode = pswdElement.form;
+                usrField = formNode.querySelectorAll("input[type='text']");
+                alert(usrField);
+            }
+            alert("usrField:" + usrField);
+            if ((doc !== undefined) && (doc !== null)) {
+                usrField.forEach(function(usrElement) {
+
+                    alert("iframe: " + doc.getElementById(usrElement.id));
+                });
+            } else {
+                usrField.forEach(function(usrElement) {
                     usrElement.value = 'USRPLACEHOLDER';
-                }
-                usrId = usrElement.id;
-                usrElement.dispatchEvent(evt);
-            });
+                    //usrId = usrElement.id;
+                    //usrElement.dispatchEvent(evt);
+                });
+            }
             pswdElement.dispatchEvent(evt);
         });
     }
 }
 
 var frames = window.frames;
-var i;
 
 if (frames.length != 0) {
-    for (i = 0; i < frames.length; i++) {
-        setLoginFields(frames[i].id);
+    for (var i = 0; i < frames.length; i++) {
+        setLoginFields(frames[i].frameElement.id);
     }
 } else {
     setLoginFields(null);
