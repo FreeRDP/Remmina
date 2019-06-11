d8547f93984baaedd91a9a041371433618b77361
Antenore Gatta <antenore@simbiosi.org>
2019-06-12 00:00:01 +0200

    Do not ignore window object when iframe has bo login elements

--- plugins/www/resources/js/www-js.js
+++ plugins/www/resources/js/www-js.js
@@ -1,46 +1,51 @@
 /*jshint esversion: 6 */
 function setLoginFields() {
-  var evt = new Event('change');
+    var evt = new Event('change');
 
-  var frames = window.frames;
+    var frames = window.frames;
 
-  var doc;
-  var pswdField;
-  var usrField;
-  var formNode;
+    var doc;
+    var pswdField;
+    var usrField;
+    var formNode;
 
-  if (frames.length != 0) {
-    for (var i = 0; i < frames.length; i++) {
-      doc = frames[i].document;
-      pswdField = doc.querySelectorAll("input[type='password']");
-      if ((pswdField !== undefined) && (pswdField !== null)) {
-        break;
-      }
+    if (frames.length != 0) {
+        for (var i = 0; i < frames.length; i++) {
+            doc = frames[i].document;
+            pswdField = doc.querySelectorAll("input[type='password']");
+            if ((pswdField !== undefined) && (pswdField !== null)) {
+                break;
+            }
+
+        }
+        if ((pswdField === undefined) || (pswdField === null)) {
+            /* What if we don't have login forms in the iFrame? -> window */
+            doc = window.document;
+            pswdField = doc.querySelectorAll("input[type='password']");
+        }
+    } else {
+        doc = window.document;
+        pswdField = doc.querySelectorAll("input[type='password']");
 
     }
-  } else {
-    doc = window.document;
-    pswdField = doc.querySelectorAll("input[type='password']");
-
-  }
 
 
-  if (pswdField !== undefined) {
-    pswdField.forEach(function(pswdElement) {
-      if (pswdElement.getAttribute('autocomplete') !== 'new-password') {
-        pswdElement.value = 'PWDPLACEHOLDER';
-      }
+    if (pswdField !== undefined) {
+        pswdField.forEach(function(pswdElement) {
+            if (pswdElement.getAttribute('autocomplete') !== 'new-password') {
+                pswdElement.value = 'PWDPLACEHOLDER';
+            }
 
-      formNode = pswdElement.form;
-      usrField = formNode.querySelectorAll("input[type='text']");
+            formNode = pswdElement.form;
+            usrField = formNode.querySelectorAll("input[type='text']");
 
-      usrField.forEach(function(usrElement) {
-        usrElement.value = 'USRPLACEHOLDER';
-        usrElement.dispatchEvent(evt);
-      });
-      pswdElement.dispatchEvent(evt);
-    });
-  }
+            usrField.forEach(function(usrElement) {
+                usrElement.value = 'USRPLACEHOLDER';
+                usrElement.dispatchEvent(evt);
+            });
+            pswdElement.dispatchEvent(evt);
+        });
+    }
 }
 
 
