329b6ef526ac0930b62ce5f3c6fe91b6594de154
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2019-06-24 23:09:29 +0200

    Unfortunately, Remmina capitalizes plugin names, Let's at least at some places spell X2Go correctly.

--- plugins/x2go/x2go_plugin.h
+++ plugins/x2go/x2go_plugin.h
@@ -40,6 +40,6 @@
 #pragma once
 
 #define PLUGIN_NAME        "X2GO"
-#define PLUGIN_DESCRIPTION "X2GO - Launch an X2Go connection"
+#define PLUGIN_DESCRIPTION "X2Go - Launch an X2Go session"
 #define PLUGIN_VERSION     VERSION      /* Remmina version */
 #define PLUGIN_APPICON     "remmina-x2go"
