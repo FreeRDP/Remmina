a2b3b1d7bf3e3884afaf470b619fe46da8fc357c
Allan Nordhøy <epost@anotheragency.no>
2020-01-26 21:03:42 +0000

    Parameters passed individually, notation, "Press any key to continue…"

--- src/external_tools/functions.sh
+++ src/external_tools/functions.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-# Waits for user key press.
+# Awaits user key press.
 pause ()
 {
- echo "Hit a key to continue…"
- OLDCONFIG=`stty -g`
+ echo "Press any key to continue…"
+ OLDCONFIG=$(stty -g)
  stty -icanon -echo min 1 time 0
  dd count=1 2>/dev/null
  stty $OLDCONFIG
 }
 
-# set terminal title for gnome-terminal and many others
+# Set terminal title for gnome-terminal and many others
 settitle() {
-  echo -ne "\033]0;${remmina_term_title}\007"
+  echo -n -e "\033]0;${remmina_term_title}\007"
 }
-
