f7c10c8de7d990569d2288eef5bc27688fa68067
Antenore Gatta <antenore@simbiosi.org>
2020-03-16 23:57:26 +0100

    WWW: javascript - fix test

--- plugins/www/resources/js/www-js.js
+++ plugins/www/resources/js/www-js.js
@@ -1,98 +1,97 @@
 /* jshint esversion: 6 */
 
 /** A quick function to find  and fill login fields */
-function setLoginFields() {
-  const evt = new Event('change');
+function setLoginFields () {
+  const evt = new Event('change')
 
-  const frames = window.frames;
+  const frames = window.frames
 
-  let doc;
-  let pswdField;
-  let usrField;
-  let formNode;
-  let userFound = false;
+  let doc
+  let pswdField
+  let usrField
+  let formNode
+  let userFound = false
 
-  if (frames.length != 0) {
+  if (frames.length !== 0) {
     for (let i = 0; i < frames.length; i++) {
-      doc = frames[i].document;
-      pswdField = doc.querySelectorAll('input[type=\'password\']');
+      doc = frames[i].document
+      pswdField = doc.querySelectorAll('input[type=\'password\']')
       if ((pswdField !== undefined) && (pswdField !== null)) {
-        break;
+        break
       }
     }
     if ((pswdField === undefined) || (pswdField === null)) {
       /* What if we don't have login forms in the iFrame? -> window */
-      doc = window.document;
-      pswdField = doc.querySelectorAll('input[type=\'password\']');
+      doc = window.document
+      pswdField = doc.querySelectorAll('input[type=\'password\']')
     }
   } else {
-    doc = window.document;
-    pswdField = doc.querySelectorAll('input[type=\'password\']');
+    doc = window.document
+    pswdField = doc.querySelectorAll('input[type=\'password\']')
   }
 
-
   if (pswdField !== undefined) {
-    pswdField.forEach(function(pswdElement) {
+    pswdField.forEach(function (pswdElement) {
       if (pswdElement.getAttribute('autocomplete') !== 'new-password') {
-        pswdElement.value = 'PWDPLACEHOLDER';
+        pswdElement.value = 'PWDPLACEHOLDER'
       }
 
-      formNode = pswdElement.form;
+      formNode = pswdElement.form
       if (formNode !== null) {
-        console.debug('Form elements found');
-        usrField = formNode.querySelectorAll('input[type=\'text\']');
+        console.debug('Form elements found')
+        usrField = formNode.querySelectorAll('input[type=\'text\']')
 
-        usrField.forEach(function(usrElement) {
-          usrElement.value = 'USRPLACEHOLDER';
+        usrField.forEach(function (usrElement) {
+          usrElement.value = 'USRPLACEHOLDER'
           if (usrElement !== null) {
-            usrElement.dispatchEvent(evt);
-            userFound = true;
+            usrElement.dispatchEvent(evt)
+            userFound = true
           }
-        });
-        pswdElement.dispatchEvent(evt);
+        })
+        pswdElement.dispatchEvent(evt)
       }
       if (formNode === null || !userFound) {
-        console.debug('Form elements found');
-        console.debug('Inputs elements may be in other containers');
-        const inputs = doc.getElementsByTagName('input');
+        console.debug('Form elements found')
+        console.debug('Inputs elements may be in other containers')
+        const inputs = doc.getElementsByTagName('input')
         for (let i = 0; i < inputs.length; i += 1) {
-          console.debug('input type: ' + inputs[i].type);
+          console.debug('input type: ' + inputs[i].type)
           switch (inputs[i].type) {
             case 'new-password':
-              continue;
+              continue
             case 'password':
-              continue;
+              continue
             case 'hidden':
-              continue;
+              continue
             case 'email':
-              inputs[i].value = 'USRPLACEHOLDER';
-              userFound = true;
-              break;
+              inputs[i].value = 'USRPLACEHOLDER'
+              userFound = true
+              break
             case 'text':
               if (!userFound) {
-                inputs[i].value = 'USRPLACEHOLDER';
-                userFound = true;
+                inputs[i].value = 'USRPLACEHOLDER'
+                userFound = true
               }
-              break;
+              break
             default:
-              console.debug('Tentativily add username if no userFound');
+              console.debug('Tentativily add username if no userFound')
               if (!userFound) {
-                inputs[i].value = 'USRPLACEHOLDER';
-                userFound = true;
+                inputs[i].value = 'USRPLACEHOLDER'
+                userFound = true
               }
                             // code block
           }
           if (userFound) {
-            inputs[i].dispatchEvent(evt);
-            console.debug('Username field found and set(?)');
-            break;
+            inputs[i].dispatchEvent(evt)
+            console.debug('Username field found and set(?)')
+            break
           }
         }
       }
-    });
+    })
   } else {
-    return;
+
   }
 }
 
-setLoginFields();
+setLoginFields()
