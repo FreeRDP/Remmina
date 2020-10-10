bcf62e4a5202bdb863ec54a4eeea4fd5c25e54cb
ToolsDevler <info@toolsdevler.net>
2020-10-10 19:48:21 +0200

    WIP: Fixed plugin loading

--- src/remmina_plugin_python.c
+++ src/remmina_plugin_python.c
@@ -13,6 +13,7 @@
 #include "remmina_plugin_python_module.h"
 #include "remmina_plugin_python_plugin.h"
 
+
 void remmina_plugin_python_init(void) {
     TRACE_CALL(__FUNC__);
     // Initialize Python environment
@@ -20,14 +21,33 @@ void remmina_plugin_python_init(void) {
     remmina_plugin_python_module_register_object(plugin_object);
     remmina_plugin_python_module_init();
 
-
     Py_Initialize();
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append('" REMMINA_RUNTIME_PLUGINDIR "')");
 }
 
-static char* basename(const char* path) {
-    return "toolsdevler";
+int basename_no_ext(const char* in, char** out) {
+    // Find last segment of path...
+    const char* end = in + strlen(in);
+    const char* from = in;
+    const char* to = end;
+    for (const char* ptr = in; ptr != end; ++ptr) {
+        if (*ptr == '/') {
+            while (*ptr == '/' && ptr != end)
+                ++ptr;
+            if (ptr == end)
+                return NULL;
+            from = ptr;
+        } else if (*ptr == '.') {
+            to = ptr;
+        }
+    }
+
+    int length = to - from;
+    *out = (char*)malloc(sizeof(char*) * ((length) + 1));
+    strncpy(*out, from, to-from);
+    (*out)[length] = '\0';
+    return length;
 }
 
 gboolean
@@ -36,14 +56,15 @@ remmina_plugin_python_load(RemminaPluginService* service, const char* name) {
     PyObject *plugin_name, *plugin_file;
     PyObject *pArgs, *pValue;
     int i;
-    
-    plugin_name = PyUnicode_DecodeFSDefault(basename(name));
+    char* filename = NULL;
+    basename_no_ext(name, &filename);
+    plugin_name = PyUnicode_DecodeFSDefault(filename);
     plugin_file = PyImport_Import(plugin_name);
     if (plugin_file != NULL) {
         return TRUE;
     } else {
-        PyErr_Print();
         g_print("Failed to load \"%s\"\n", name);
+        PyErr_Print();
         return FALSE;
     }
     return TRUE;
