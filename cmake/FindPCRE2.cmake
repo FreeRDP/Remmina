baffad0335f14252b3f59b275535c53971ed5543
Matteo F. Vescovi <mfvescovi+gitlab@gmail.com>
2020-10-20 15:40:00 +0000

    Homogenize FindPCRE2.cmake to other .cmake files

--- cmake/FindPCRE2.cmake
+++ cmake/FindPCRE2.cmake
@@ -1,3 +1,29 @@
+# Remmina - The GTK+ Remote Desktop Client
+#
+# Copyright (C) 2011 Marc-Andre Moreau
+# Copyright (C) 2014-2015 Antenore Gatta, Fabio Castelli, Giovanni Panozzo
+# Copyright (C) 2016-2020 Antenore Gatta, Giovanni Panozzo
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA  02110-1301, USA.
+
+include(FindPackageHandleStandardArgs)
+
+pkg_check_modules(PC_LIBPCRE2 libpcre2-8-0)
+
+
 find_path(PCRE2_INCLUDE_DIR
   NAMES pcre2.h
   PATHS ../../../../libs
