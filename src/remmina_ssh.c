ea3f232f2e8554e81a6be41ab1785a134798e9e1
Antenore Gatta <antenore@simbiosi.org>
2021-03-26 14:06:02 +0100

    Disable cert file auth when libssh < 0.9.0
    Closes #2441
    
    Signed-off-by: Antenore Gatta <antenore@simbiosi.org>

--- src/remmina_ssh.c
+++ src/remmina_ssh.c
@@ -314,7 +314,8 @@ remmina_ssh_auth_pubkey(RemminaSSH *ssh, RemminaProtocolWidget *gp, RemminaFile 
 	REMMINA_DEBUG("SSH certificate file: %s", ssh->certfile);
 	REMMINA_DEBUG("SSH private key file: %s", ssh->privkeyfile);
 	if (ssh->certfile != NULL) {
-		/* First we import the certificate */
+#if LIBSSH_VERSION_INT >= SSH_VERSION_INT(0, 9, 0)
+		/* First we import the private key */
 		if (ssh_pki_import_privkey_file(ssh->privkeyfile, (ssh->passphrase ? ssh->passphrase : ""),
 						NULL, NULL, &key) != SSH_OK) {
 			if (ssh->passphrase == NULL || ssh->passphrase[0] == '\0') {
@@ -327,7 +328,8 @@ remmina_ssh_auth_pubkey(RemminaSSH *ssh, RemminaProtocolWidget *gp, RemminaFile 
 			return REMMINA_SSH_AUTH_AUTHFAILED_RETRY_AFTER_PROMPT;
 		}
 		REMMINA_DEBUG ("Imported private SSH key file");
-		ret = ssh_pki_import_cert_file 	(ssh->certfile, &cert ) 	;
+		/* First we import the certificate */
+		ret = ssh_pki_import_cert_file(ssh->certfile, &cert );
 		if (ret != SSH_OK) {
 			REMMINA_DEBUG ("Certificate import returned: %d", ret);
 			// TRANSLATORS: The placeholder %s is an error message
@@ -356,6 +358,9 @@ remmina_ssh_auth_pubkey(RemminaSSH *ssh, RemminaProtocolWidget *gp, RemminaFile 
 			return REMMINA_SSH_AUTH_FATAL_ERROR;
 		}
 		REMMINA_DEBUG ("Authentication with a certificate file works, we can authenticate");
+#else
+		REMMINA_DEBUG ("lbssh >= 0.9.0 is required to authenticate with certificate file");
+#endif
 		/* if it goes well we authenticate (later on) with the key, not the cert*/
 	} else {
 		if (ssh->privkeyfile == NULL) {
