b00fae2911d133850d4ee04fee91c25c66e2b4f6
Antenore Gatta <antenore@simbiosi.org>
2021-05-28 13:31:19 +0200

    Minor fixes for v1.4.17
    - RDP: MIssing dynamic channel rdpgfx
    - Icon: New panel icon
    
    Signed-off-by: Antenore Gatta <antenore@simbiosi.org>

--- src/remmina_icon.c
+++ src/remmina_icon.c
@@ -429,7 +429,7 @@ void remmina_icon_init(void)
 	gboolean sni_supported;
 	char msg[200];
 
-	g_stpcpy(remmina_panel, "org.remmina.Remmina-symbolic");
+	g_stpcpy(remmina_panel, "remmina-panel");
 
 	/* Print on stdout the availability of appindicators on DBUS */
 	sni_supported = remmina_sysinfo_is_appindicator_available();
@@ -459,7 +459,7 @@ void remmina_icon_init(void)
 	if (!remmina_icon.icon && !remmina_pref.disable_tray_icon) {
 #ifdef HAVE_LIBAPPINDICATOR
 		remmina_icon.icon = app_indicator_new("remmina-icon", remmina_panel, APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
-		app_indicator_set_icon_theme_path(remmina_icon.icon, REMMINA_RUNTIME_DATADIR G_DIR_SEPARATOR_S "icons/hicolor/apps");
+		app_indicator_set_icon_theme_path(remmina_icon.icon, REMMINA_RUNTIME_DATADIR G_DIR_SEPARATOR_S "icons");
 
 		app_indicator_set_status(remmina_icon.icon, APP_INDICATOR_STATUS_ACTIVE);
 		app_indicator_set_title(remmina_icon.icon, "Remmina");
