FreeRDP/Remmina

commit 7c13b918b409ab0342829cb9d28920472b4bccca

Added setting for RDP number of reconnect attempts
- General settings for all connection profiles
- Setting for a specific connection profile
- If not set it uses the FreeRDP default that is 20
- Connection profile setting have precedence to the general settings

Should fix #2079

Signed-off-by: Antenore Gatta <antenore@simbiosi.org>

plugins/rdp/rdp_plugin.c
@@ -381,8 +381,13 @@ BOOL rf_auto_reconnect(rfContext *rfi)
 	RemminaPluginRdpUiObject *ui;
 	time_t treconn;
 
+	RemminaProtocolWidget *gp = rfi->protocol_widget;
+	RemminaFile *remminafile = remmina_plugin_service->protocol_plugin_get_file(gp);
+
 	rfi->is_reconnecting = TRUE;
-	rfi->reconnect_maxattempts = freerdp_settings_get_uint32(settings, FreeRDP_AutoReconnectMaxRetries);
+	gint i = atoi(remmina_plugin_service->pref_get_value("rdp_reconnect_attempts"));
+	i = remmina_plugin_service->file_get_int(remminafile, "rdp_reconnect_attempts", i);
+	rfi->reconnect_maxattempts = (i && i >= 0 ? i : freerdp_settings_get_uint32(settings, FreeRDP_AutoReconnectMaxRetries));
 	rfi->reconnect_nattempt = 0;
 
 	/* Only auto reconnect on network disconnects. */
@@ -2614,6 +2619,7 @@ static const RemminaProtocolSetting remmina_rdp_basic_settings[] =
 	{ REMMINA_PROTOCOL_SETTING_TYPE_SELECT,	    "colordepth",		N_("Colour depth"),			  FALSE, colordepth_list, NULL									},
 	{ REMMINA_PROTOCOL_SETTING_TYPE_SELECT,	    "network",			N_("Network connection type"),		  FALSE, network_list,	  network_tooltip							},
 	{ REMMINA_PROTOCOL_SETTING_TYPE_FOLDER,	    "sharefolder",		N_("Share folder"),			  FALSE, NULL,		  NULL									},
+	{ REMMINA_PROTOCOL_SETTING_TYPE_TEXT,	    "rdp_reconnect_attempts",	N_("Reconnect attempts number"),	  FALSE, NULL, N_("The maximum number of reconnect attempts upon an RDP disconnect (default: 20)")									},
 	{ REMMINA_PROTOCOL_SETTING_TYPE_END,	    NULL,			NULL,					  FALSE, NULL,		  NULL									}
 };
 

plugins/rdp/rdp_settings.c
@@ -95,6 +95,7 @@ typedef struct _RemminaPluginRdpsetGrid {
 	GtkWidget *	composition_check;
 	GtkWidget *	use_client_keymap_check;
 	GtkWidget *	disable_smooth_scrolling_check;
+	GtkWidget *	reconnect_attempts;
 
 	/* FreeRDP /scale-desktop: Scaling of desktop app */
 	GtkWidget *	desktop_scale_factor_spin;
@@ -150,6 +151,9 @@ static void remmina_rdp_settings_grid_destroy(GtkWidget *widget, gpointer data)
 
 	remmina_plugin_service->pref_set_value("rdp_disable_smooth_scrolling",
 					       gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(grid->disable_smooth_scrolling_check)) ? "1" : "0");
+
+	remmina_plugin_service->pref_set_value("rdp_reconnect_attempts",
+					       gtk_entry_get_text(GTK_ENTRY(grid->reconnect_attempts)));
 
 	s = g_strdup_printf("%X", grid->quality_values[0]);
 	remmina_plugin_service->pref_set_value("rdp_quality_0", s);
@@ -600,6 +604,25 @@ static void remmina_rdp_settings_grid_init(RemminaPluginRdpsetGrid *grid)
 	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(widget),
 				     s && s[0] == '1' ? TRUE : FALSE);
 	g_free(s);
+
+	widget = gtk_label_new(_("General settings"));
+	gtk_widget_show(widget);
+	gtk_widget_set_halign(GTK_WIDGET(widget), GTK_ALIGN_START);
+	gtk_widget_set_valign(GTK_WIDGET(widget), GTK_ALIGN_CENTER);
+	gtk_grid_attach(GTK_GRID(grid), widget, 0, 31, 1, 1);
+	widget = gtk_label_new(_("Reconnect attempts number"));
+	gtk_grid_attach(GTK_GRID(grid), widget, 1, 31, 1, 1);
+	widget = gtk_entry_new();
+	gtk_grid_attach(GTK_GRID(grid), widget, 2, 31, 2, 1);
+	gtk_entry_set_input_purpose(GTK_ENTRY(widget), GTK_INPUT_PURPOSE_NUMBER);
+	gtk_entry_set_input_hints(GTK_ENTRY(widget), GTK_INPUT_HINT_NONE);
+	gtk_widget_set_tooltip_text(widget, _("The maximum number of reconnect attempts upon an RDP disconnect (default: 20)"));
+	s = remmina_plugin_service->pref_get_value("rdp_reconnect_attempts");
+	if (s && s[0])
+		gtk_entry_set_text(GTK_ENTRY(widget), s);
+	g_free(s);
+	grid->reconnect_attempts = widget;
+
 }
 
 GtkWidget *remmina_rdp_settings_new(void)

commit ba680f185c57902191b0630846fd837bb9a470d0

Add RDP reconnect interrupt on window close, fix crash introduced with 7c13b918. Should fix #2079

plugins/rdp/rdp_plugin.c
@@ -380,14 +380,22 @@ BOOL rf_auto_reconnect(rfContext *rfi)
 	rdpSettings *settings = rfi->instance->settings;
 	RemminaPluginRdpUiObject *ui;
 	time_t treconn;
+	gchar *cval;
+	gint maxattempts;
 
 	RemminaProtocolWidget *gp = rfi->protocol_widget;
 	RemminaFile *remminafile = remmina_plugin_service->protocol_plugin_get_file(gp);
 
 	rfi->is_reconnecting = TRUE;
-	gint i = atoi(remmina_plugin_service->pref_get_value("rdp_reconnect_attempts"));
-	i = remmina_plugin_service->file_get_int(remminafile, "rdp_reconnect_attempts", i);
-	rfi->reconnect_maxattempts = (i && i >= 0 ? i : freerdp_settings_get_uint32(settings, FreeRDP_AutoReconnectMaxRetries));
+	rfi->stop_reconnecting_requested = FALSE;
+
+	maxattempts = FreeRDP_AutoReconnectMaxRetries;
+	if ((cval = remmina_plugin_service->pref_get_value("rdp_reconnect_attempts")) != NULL)
+		maxattempts = atoi(cval);
+	maxattempts = remmina_plugin_service->file_get_int(remminafile, "rdp_reconnect_attempts", maxattempts);
+	if (maxattempts <= 0)
+		maxattempts = freerdp_settings_get_uint32(settings, FreeRDP_AutoReconnectMaxRetries);
+	rfi->reconnect_maxattempts = maxattempts;
 	rfi->reconnect_nattempt = 0;
 
 	/* Only auto reconnect on network disconnects. */
@@ -433,6 +441,12 @@ BOOL rf_auto_reconnect(rfContext *rfi)
 			break;
 		}
 
+		if (rfi->stop_reconnecting_requested) {
+			REMMINA_PLUGIN_DEBUG("[%s] reconnect request loop interrupted by user.",
+					freerdp_settings_get_string(rfi->settings, FreeRDP_ServerHostname));
+			break;
+		}
+
 		/* Attempt the next reconnect */
 		REMMINA_PLUGIN_DEBUG("[%s] reconnection, attempt #%d of %d",
 				     freerdp_settings_get_string(rfi->settings, FreeRDP_ServerHostname), rfi->reconnect_nattempt, rfi->reconnect_maxattempts);
@@ -456,9 +470,12 @@ BOOL rf_auto_reconnect(rfContext *rfi)
 			}
 		}
 
-		/* Wait until 5 secs have elapsed from last reconnect attempt */
-		while (time(NULL) - treconn < 5)
-			sleep(1);
+		/* Wait until 5 secs have elapsed from last reconnect attempt, while checking for rfi->stop_reconnecting_requested */
+		while (time(NULL) - treconn < 25) {
+			if (rfi->stop_reconnecting_requested)
+				break;
+			usleep(200000); // 200ms sleep
+		}
 	}
 
 	rfi->is_reconnecting = FALSE;
@@ -2259,6 +2276,7 @@ static void remmina_rdp_init(RemminaProtocolWidget *gp)
 	rfi->settings = instance->settings;
 	rfi->connected = False;
 	rfi->is_reconnecting = False;
+	rfi->stop_reconnecting_requested = False;
 	rfi->user_cancelled = FALSE;
 
 	freerdp_register_addin_provider(freerdp_channels_load_static_addin_entry, 0);
@@ -2341,6 +2359,12 @@ static gboolean remmina_rdp_close_connection(RemminaProtocolWidget *gp)
 		/* Allow clipboard transfer from server to terminate */
 		rfi->clipboard.srv_clip_data_wait = SCDW_ABORTING;
 		usleep(100000);
+	}
+
+	if (rfi->is_reconnecting) {
+		/* Special case: window closed when attempting to reconnect */
+		rfi->stop_reconnecting_requested = TRUE;
+		return FALSE;
 	}
 
 	rdp_event.type = REMMINA_RDP_EVENT_DISCONNECT;

plugins/rdp/rdp_plugin.h
@@ -308,6 +308,7 @@ struct rf_context {
 
 	gboolean		connected;
 	gboolean		is_reconnecting;
+	gboolean		stop_reconnecting_requested;
 	/* orphaned: rf_context has still one or more libfreerdp thread active,
 	 * but no longer maintained by an open RemminaProtocolWidget/tab.
 	 * When the orphaned thread terminates, we must cleanup rf_context.