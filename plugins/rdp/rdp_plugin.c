35d66b57a8f472821bd7e00ab7f6c02bb39d7f76
myheroyuki <myheroyuki@outlook.com>
2024-05-04 00:55:51 +0900

    [REM-3104] Added back in line that was accidentally removed before merge

--- plugins/rdp/rdp_plugin.c
+++ plugins/rdp/rdp_plugin.c
@@ -2710,6 +2710,7 @@ static void remmina_rdp_init(RemminaProtocolWidget *gp)
 
 	freerdp_register_addin_provider(freerdp_channels_load_static_addin_entry, 0);
 #if FREERDP_VERSION_MAJOR >= 3
+	auth_list = remmina_plugin_service->pref_get_value("rdp_auth_filter");
 	if (auth_list != NULL && auth_list[0] != 0){
 		freerdp_settings_set_string(rfi->clientContext.context.settings, FreeRDP_AuthenticationPackageList, auth_list);
 	}
