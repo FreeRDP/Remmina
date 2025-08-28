02c6a5732d135883853a268136f17b50dd901bc0
myheroyuki <myheroyuki@outlook.com>
2025-08-28 21:17:17 +0900

    Removed default.nix

--- default.nix
+++
@@ -1,8 +0,0 @@
-#to use in nix, use "nix-build"
-let
-  pkgs = import <nixpkgs> {};
-in
-  pkgs.remmina.overrideAttrs (oldAttrs: {
-    # Use the source code in the current directory.
-    src = ./.;
-  })
